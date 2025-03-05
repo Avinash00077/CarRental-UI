43dec609b0b352f0d31b70f5cc96e1f23e98ab43
Avinash Reddy <avinashreddytummuri77@gmail.com>
2025-03-05 13:33:24 +0530

    onclick image

--- src/components/courosel/Courosel.jsx
+++ src/components/courosel/Courosel.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "slick-carousel/slick/slick.css"; // Import Slick CSS
 import "slick-carousel/slick/slick-theme.css"; // Import Slick Theme CSS
 import Slider from "react-slick";
 import { useScreenSize } from "../../context/screenSizeContext";
-import car50 from '../../assets/car_50.png'
+import { getUserToken } from "../../utils/getToken";
+import car50 from "../../assets/car_50.png";
 import "./Courosel.css";
 
 const Courosel = () => {
   const { isScreenSmall } = useScreenSize();
+  const navigate = useNavigate();
   const settings = {
     dots: true,
     infinite: true,
@@ -17,6 +20,31 @@ const Courosel = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false, // Disable previous/next arrows
+  };
+
+  const authToken = getUserToken();
+  const location = localStorage.getItem("location") || 'Hyderabad';
+  console.log()
+  const handleImageClick = () => {
+    // Get current date
+    const today = new Date();
+
+    // Format to YYYY-MM-DD
+    const pickUpDate = today.toISOString().split("T")[0];
+    const pickUpTime = "10:00";
+    // Calculate drop-off date (6 days from today)
+    const dropOffDate = new Date();
+    dropOffDate.setDate(today.getDate() + 30);
+    const dropOffTime = "10:00";
+    const formattedDropOffDate = dropOffDate.toISOString().split("T")[0];
+
+    if (authToken && location) {
+      navigate(
+        `/viewCars?pickUpDate=${pickUpDate}&toDate=${formattedDropOffDate}&location=${location}&pickupTime=${pickUpTime}&dropoffTime=${dropOffTime}`
+      );
+    } else {
+      window.location.href = "/auth";
+    }
   };
 
   return (
@@ -34,6 +62,7 @@ const Courosel = () => {
               } rounded-xl`}
               src="https://ideogram.ai/assets/progressive-image/balanced/response/efRj0Q6lT820C3GM8MclEg"
               alt="slide 1"
+              onClick={() => handleImageClick()}
             />
           </div>
           <div>
@@ -43,6 +72,7 @@ const Courosel = () => {
               } rounded-xl`}
               src={car50}
               alt="slide 2"
+              onClick={() => handleImageClick()}
             />
           </div>
           <div>
