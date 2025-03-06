26b982d5cb6993f59cd7d0ac9e1a8dcc9045ce77
Avinash Reddy <avinashreddytummuri77@gmail.com>
2025-03-06 10:47:30 +0530

    ride fixes

--- src/components/RideCars.jsx
+++ src/components/RideCars.jsx
@@ -3,22 +3,22 @@ import { useNavigate } from "react-router-dom";
 
 const RideCars = ()=>{
     const pickUpDate = new Date();
-    console.log(pickUpDate,"jddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-    const dropOffDate =pickUpDate.getDate()+2
-    console.log(dropOffDate," Heeloeeeeeeeeeeeee")
+
+const dropOffDate = new Date(pickUpDate); // Clone the date
+dropOffDate.setDate(pickUpDate.getDate() + 2); // Add 2 days
+
     const selectedLocation =localStorage.getItem("location")||"Hyderabad"
-    console.log(selectedLocation,"jjjjj")
     const pickUpTime ="10:00"
     const dropOffTime="11:00"
     const navigate = useNavigate();
     useEffect(()=>{
         navigate(
-            `/viewCars?pickUpDate=${pickUpDate}&toDate=${pickUpDate}&location=${selectedLocation}&pickupTime=${pickUpTime}&dropoffTime=${dropOffTime}`
+            `/viewCars?pickUpDate=${pickUpDate}&toDate=${dropOffDate}&location=${selectedLocation}&pickupTime=${pickUpTime}&dropoffTime=${dropOffTime}`
           );
     },[])
     return(
         <div>
-            <h1>Hello Sudheer</h1>
+            <h1>Loading .............</h1>
         </div>
     )
 }
